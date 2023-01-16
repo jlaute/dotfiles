09c2bc774021edb3821b709b17998a1c2bd81a05
Jörg Lautenschlager <mail@lautenschlagerj.de>
2023-01-17 00:35:13 +0100

    added i3 config

--- install.sh
+++ install.sh
@@ -6,3 +6,6 @@ ln -sf "$HOME/dotfiles/nvim/init.vim" "$HOME/.config/nvim"
 
 rm -rf "$HOME/.config/X11"
 ln -s "$HOME/dotfiles/X11" "$HOME/.config"
+
+rm -rf "$HOME/.config/i3"
+ln -s "$HOME/dotfiles/i3" "$HOME/.config"
